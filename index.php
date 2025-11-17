2367aa36e9c0f356c46ac83059eec5ad96806d94
Syamil CA <65897749+MilQ28@users.noreply.github.com>
2025-11-17 13:35:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
 <body>
     <h2>Data Alumni Sekolah</h2>
     <a href="tambah.php" id="tambahdata">+ Tambah Data</a>
+
+    <!-- FORM PENCARIAN -->
+    <form method="GET" style="margin-bottom: 20px;">
+        <input type="text" name="cari" placeholder="Cari nama / NIK / NISN / Jurusan..."
+            value="<?= isset($_GET['cari']) ? $_GET['cari'] : '' ?>">
+        <button type="submit">Cari</button>
+    </form>
+
     <table>
         <tr>
             <th>ID</th>
@@ -23,24 +31,38 @@
             <th>Jurusan</th>
             <th>Perubahan</th>
         </tr>
+
         <?php
-        $result = mysqli_query($conn, "SELECT * FROM alumni");
+        // PENCARIAN
+        if (isset($_GET['cari'])) {
+            $cari = $_GET['cari'];
+            $result = mysqli_query($conn, "SELECT * FROM alumni 
+                WHERE nama LIKE '%$cari%' 
+                OR nik LIKE '%$cari%' 
+                OR nisn LIKE '%$cari%' 
+                OR jurusan LIKE '%$cari%' 
+                OR tahun_lulus LIKE '%$cari%' ");
+        } else {
+            $result = mysqli_query($conn, "SELECT * FROM alumni");
+        }
+
+        // TAMPILKAN DATA
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr>
-        <td>{$row['id']}</td>
-        <td>{$row['nama']}</td>
-        <td>{$row['nik']}</td>
-        <td>{$row['nisn']}</td>
-        <td>{$row['tempat_lahir']}</td>
-        <td>{$row['tanggal_lahir']}</td>
-        <td>{$row['alamat']}</td>
-        <td>{$row['tahun_lulus']}</td>
-        <td>{$row['jurusan']}</td>
-        <td>
-          <a href='edit.php?id={$row['id']}'>Edit</a> |
-          <a href='hapus.php?id={$row['id']}' onclick=\"return confirm('Yakin ingin hapus?')\">Hapus</a>
-        </td>
-      </tr>";
+                <td>{$row['id']}</td>
+                <td>{$row['nama']}</td>
+                <td>{$row['nik']}</td>
+                <td>{$row['nisn']}</td>
+                <td>{$row['tempat_lahir']}</td>
+                <td>{$row['tanggal_lahir']}</td>
+                <td>{$row['alamat']}</td>
+                <td>{$row['tahun_lulus']}</td>
+                <td>{$row['jurusan']}</td>
+                <td>
+                    <a href='edit.php?id={$row['id']}'>Edit</a> |
+                    <a href='hapus.php?id={$row['id']}' onclick=\"return confirm('Yakin ingin hapus?')\">Hapus</a>
+                </td>
+            </tr>";
         }
         ?>
     </table>
